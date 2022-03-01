1fd15cdd7f71e766d39f04ed59a2182afc7d4be2
Indirav123 <Indira.Vundrakonda@ibm.com>
2022-03-01 14:36:03 +0530

    Add project files.

---
+++ naturalnumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleapp1
+{
+    internal class naturalnumbers
+    {
+        static void Main(string[] args)
+        {
+            int i, sum = 0, n;
+            Console.WriteLine("Enter the nth number:");
+            n = int.Parse(Console.ReadLine());
+            for (i = 0; i <= n; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine("sum of n numbers:" + sum);
+            Console.ReadLine();
+        }
+    }
+}
